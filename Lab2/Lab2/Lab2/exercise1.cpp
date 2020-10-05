a1176791ddff6b7af26cd6422634b79405b86f97
QuinnF19 <qfryday10@hotmail.com>
2020-10-05 13:49:49 -0400

    I added my parts, maybe we can compign part 3

---
+++ Lab2/Lab2/Lab2/exercise1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "exercise1.h"
+using namespace std;
+
+//Exercise 1 : (2 Marks)
+//Modify the following program, just by inserting the display operator, in such a way that it
+//displays the following values::
+
+//void main(void)
+//{
+	//int tab[80];
+	//int* p;
+	//int i;
+	//for (i = 0; i < 80; i++) tab[i] = i * i;
+	//cout << tab[2];
+	//tab[2] = tab[1];
+	//cout << tab[1];
+	//cout << tab[2];
+	//tab[2] = *(tab + 1);
+	//cout << tab[2];
+	//*(tab + 2) = tab[1];
+	//cout << *(tab + 2);
+	//*(tab + 2) = *(tab + 1);
+	//cout << *(tab + 2);
+	//p = &tab[0];
+	//wrong spot
+	//tab[4] = *p;
+	//wrong spot
+
+
+//}
---
+++ Lab2/Lab2/Lab2/myfile2.cpp
@@ -0,0 +1,52 @@
+#include "myfile2.h"
+/********************************************************************************/
+/* Function menu which returns a character corresponding to the user's choice */
+/********************************************************************************/
+char menu(void)
+{
+	char choice;
+	cout << endl << endl << "\t\tMenu" << endl << endl;
+	cout << "1) Strings display." << endl;
+	cout << "2) Replacement of a string by an other one" << endl;
+	cout << "3) Sorting strings" << endl;
+	cout << "4) Exit of the program" << endl << endl;
+	cout << "Your choice :";
+	cin >> choice;
+	return(choice);
+}/********************************************************************************/
+/*The replace function that replaces one string by another. It takes as arguments an array of pointers
+on the strings "tab", the number of elements of this array of pointers "nbre" and the maximum size of
+the strings "size" */
+/********************************************************************************/
+void replace(char* tab[], int const& nbre, int const& size)
+{
+	int numero; //the string to modify
+	cout << endl << "Enter the string number to modify: ";
+	cin.ignore(INT_MAX, '\n');
+	cin >> numero;
+	if (numero >= 0 && numero <= nbre) //check if the number is valid
+	{
+		cout << "Enter the new string: ";
+		cin.ignore(INT_MAX, '\n');
+		cin.getline(tab[numero], size, ' ');
+	}
+	else
+	{
+		cout << "There is no string with this number" << endl;
+	}
+
+}
+void display(char* tab[], int const& nbre) {
+	int num; //the string to display
+	cout << endl << "Enter the string number to display: ";
+	cin.ignore(INT_MAX, '\n');
+	cin >> num;
+	if (num >= 0 && num <= nbre) //check if the number is valid
+	{
+		cout << tab[num];
+	}
+	else
+	{
+		cout << "There is no string with this number" << endl;
+	}
+}
